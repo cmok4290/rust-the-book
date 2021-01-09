e7f3c43188d883bc819d71195ad434ee9f719ac6
Chheany Mok <cmok4290@gmail.com>
2021-01-09 07:16:31 -0500

    Create and run a project using Cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/headnodic/Dev/rust_the_book/hello_cargo/target/debug/hello_cargo: /home/headnodic/Dev/rust_the_book/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/deps/hello_cargo-7f1a40624e4a5c8e.d
@@ -0,0 +1,5 @@
+/home/headnodic/Dev/rust_the_book/hello_cargo/target/release/deps/hello_cargo-7f1a40624e4a5c8e: src/main.rs
+
+/home/headnodic/Dev/rust_the_book/hello_cargo/target/release/deps/hello_cargo-7f1a40624e4a5c8e.d: src/main.rs
+
+src/main.rs:
